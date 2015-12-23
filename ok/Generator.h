3bcc99e32829028ea2aa0e13fe772cda3266c075
ketchupista <karolmajka95@gmail.com>
2015-12-23 23:17:49 +0100

    zmienna nr dalem jako public

--- ok/Generator.h
+++ ok/Generator.h
@@ -5,9 +5,10 @@
 using namespace std;
 class Generator
 {
-	static int nr;
+	//static int nr;
 	int dlugoscOperacji,liczbaPrzerw;
 public:
+	static int nr;
 	static const int n = 50;
 	void generuj(int seed);
 	Generator(int dlugoscOperacji, int liczbaPrzerw);
